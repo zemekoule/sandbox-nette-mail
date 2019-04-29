cac9c78fdd652d90de5f6ad528021352fc6908ef
Jaroslav Malý <jarda.maly@gmail.com>
2019-04-29 03:42:11 +0200

    Homepage: added form EmailForm

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,12 +1,25 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace App\Presenters;
 
 use Nette;
 
-
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+	/**
+	 * @return \Nette\Application\UI\Form
+	 */
+	public function createComponentEmailForm()
+	{
+		$form = new Nette\Application\UI\Form();
+
+		$form->addEmail('emailTo', 'Email');
+		$form->addTextArea('message', 'Text emailu');
+		$form->addSubmit('Odeslat');
+		$form->onSuccess[] = [$this, 'emailFormSucceeded'];
+
+		return $form;
+	}
 }
+
